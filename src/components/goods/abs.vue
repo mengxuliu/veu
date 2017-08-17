9ef308356a56b8b32456ec73c171621980621763
mengxuliu <mengxu.liu@pactera.com>
2017-08-17 23:53:18 +0800

    add li

--- src/components/goods/abs.vue
+++ src/components/goods/abs.vue
@@ -1,6 +1,25 @@
 <template>
     <div>
-
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
     </div>
 </template>
 <script>
